7b2f15dff7bb953b5cc3b8dab1efb53ce1bc3128
Dylan-Rick <dylan.rick@student.swic.edu>
2026-02-17 18:19:51 -0600

    Add personal information and introduction logging

---
+++ index.js
@@ -0,0 +1,13 @@
+// Personal Information
+const firstName = "Dylan";
+const lastName = "Rick";
+const age = 21; // Your actual age
+const isStudent = true;
+
+// Using template literals
+const introduction = `Hello! My name is ${firstName} ${lastName}. I am ${age} years old.`;
+const studentStatus = `Am I a student? ${isStudent}`;
+
+// Print to console
+console.log(introduction);
+console.log(studentStatus);
